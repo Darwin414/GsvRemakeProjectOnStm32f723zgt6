1b308bd348e4c7bbbf8a0d610d18705c6c5b873a
Xuu <Darwin414@vip.qq.com>
2026-03-23 16:45:56 +0800

    feat:增加RTT功能.

--- project/Src/main.c
+++ project/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "SEGGER_RTT.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -87,7 +87,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  SEGGER_RTT_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
